6eaca12d7aa55fd4efc475cb2982e9061334a223
Pavel Anokhov <p.anokhov@gmail.com>
2019-05-24 20:32:39 +0300

    added mutable-content property

--- SharpPusherTests/ApnsTests.cs
+++ SharpPusherTests/ApnsTests.cs
@@ -27,5 +27,27 @@ namespace SharpPusherTests
 			var serialized = JsonConvert.SerializeObject(notification, Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 			Assert.Equal(expected, serialized);
 		}
-	}
+
+        [Fact]
+        public void ApnsSerializationMutableContent()
+        {
+            var notification = new ApnsNotification
+            {
+                Payload = new ApnsPayload
+                {
+                    Alert = new ApnsNotificationAlert
+                    {
+                        BodyLocalizationKey = "GAME_PLAY_REQUEST_FORMAT",
+                        BodyLocalizationArgs = new string[] { "Jenna", "Frank" }
+                    },
+                    Sound = "chime.aiff",
+                    MutableContent = 1
+                }
+            };
+            var expected = "{\"aps\":{\"alert\":{\"loc-key\":\"GAME_PLAY_REQUEST_FORMAT\",\"loc-args\":[\"Jenna\",\"Frank\"]},\"sound\":\"chime.aiff\",\"mutable-content\":1}}";
+
+            var serialized = JsonConvert.SerializeObject(notification, Formatting.None, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            Assert.Equal(expected, serialized);
+        }
+    }
 }
